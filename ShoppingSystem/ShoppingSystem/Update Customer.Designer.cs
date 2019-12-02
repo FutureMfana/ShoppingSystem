79f761769cc452393c53878b494f3721401229d7
Kamvelihle Innocent Mjanyelwa <31170359+FutureMfana@users.noreply.github.com>
2019-12-02 22:10:49 +0200

    update customer
    update customer finally done and working properly

---
+++ ShoppingSystem/ShoppingSystem/Update Customer.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace ShoppingSystem
+{
+    partial class Update_Customer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboCustID = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtSur = new System.Windows.Forms.TextBox();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtGender = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.txtResAdd = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Customer:";
+            // 
+            // cboCustID
+            // 
+            this.cboCustID.FormattingEnabled = true;
+            this.cboCustID.Location = new System.Drawing.Point(156, 14);
+            this.cboCustID.Name = "cboCustID";
+            this.cboCustID.Size = new System.Drawing.Size(460, 24);
+            this.cboCustID.TabIndex = 1;
+            this.cboCustID.SelectedIndexChanged += new System.EventHandler(this.cboCustID_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "First Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Last Name:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 195);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "ID Number:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 254);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 17);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Gender:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 321);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(102, 17);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Email Address:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 387);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(138, 17);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Residential Address:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(156, 76);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(460, 22);
+            this.txtName.TabIndex = 8;
+            // 
+            // txtSur
+            // 
+            this.txtSur.Location = new System.Drawing.Point(156, 131);
+            this.txtSur.Name = "txtSur";
+            this.txtSur.Size = new System.Drawing.Size(460, 22);
+            this.txtSur.TabIndex = 9;
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(156, 190);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(460, 22);
+            this.txtID.TabIndex = 10;
+            // 
+            // txtGender
+            // 
+            this.txtGender.Location = new System.Drawing.Point(156, 249);
+            this.txtGender.Name = "txtGender";
+            this.txtGender.Size = new System.Drawing.Size(460, 22);
+            this.txtGender.TabIndex = 11;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(156, 318);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(460, 22);
+            this.txtEmail.TabIndex = 12;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(156, 467);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(460, 44);
+            this.btnUpdate.TabIndex = 16;
+            this.btnUpdate.Text = "&Update Customer";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // txtResAdd
+            // 
+            this.txtResAdd.Location = new System.Drawing.Point(156, 384);
+            this.txtResAdd.Name = "txtResAdd";
+            this.txtResAdd.Size = new System.Drawing.Size(460, 77);
+            this.txtResAdd.TabIndex = 17;
+            this.txtResAdd.Text = "";
+            // 
+            // Update_Customer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 528);
+            this.Controls.Add(this.txtResAdd);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtGender);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.txtSur);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cboCustID);
+            this.Controls.Add(this.label1);
+            this.Name = "Update_Customer";
+            this.Text = "Update_Customer";
+            this.Load += new System.EventHandler(this.Update_Customer_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboCustID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtSur;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtGender;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.RichTextBox txtResAdd;
+    }
+}
